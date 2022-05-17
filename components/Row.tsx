d9db452e827778e9f047feb99661bbe363981137
zzkhotivari <zuukhotivari@gmail.com>
2022-05-17 21:17:53 +0400

    feat: manipulating left and right arrows with state and ref, added styles

--- components/Row.tsx
+++ components/Row.tsx
@@ -1,3 +1,4 @@
+import { useCallback, useRef, useState } from "react";
 import { Movie } from "../typings";
 import { ChevronLeftIcon, ChevronRightIcon } from "@heroicons/react/outline";
 import Thumbnail from "./Thumbnail";
@@ -8,6 +9,24 @@ interface IProps {
 }
 
 function Row({ title, movies }: IProps) {
+  const [isMoved, setIsMoved] = useState(false);
+
+  const rowRef = useRef<HTMLDivElement>(null);
+
+  const handleClick = useCallback((direction: string) => {
+    setIsMoved(true);
+
+    if (rowRef.current) {
+      const { scrollLeft, clientWidth } = rowRef.current;
+      const scrollTo =
+        direction === "left"
+          ? scrollLeft - clientWidth
+          : scrollLeft + clientWidth;
+
+      rowRef.current.scrollTo({ left: scrollTo, behavior: "smooth" });
+    }
+  }, []);
+
   return (
     <div className="h-40 space-y-0.5 md:space-y-2">
       <h2
@@ -17,19 +36,28 @@ function Row({ title, movies }: IProps) {
         {title}
       </h2>
       <div className="group relative md:-ml-2">
-        <ChevronLeftIcon
-          className="absolute top-0 bottom-0 left-2 z-40 m-auto h-9 w-9
-         cursor-pointer opacity-0 transition hover:scale-125 group-hover:opacity-100"
-        />
-        <div className="flex items-center space-x-0.5 overflow-x-scroll md:space-x-2.5 md:p-2">
+        <button onClick={() => handleClick("left")}>
+          <ChevronLeftIcon
+            className={`absolute top-0 bottom-0 left-2 z-40 m-auto h-9 w-9 cursor-pointer opacity-0
+             transition hover:scale-125 group-hover:opacity-100 ${
+               !isMoved && "hidden"
+             }`}
+          />
+        </button>
+        <div
+          ref={rowRef}
+          className="flex items-center space-x-0.5 overflow-x-scroll scrollbar-hide md:space-x-2.5 md:p-2"
+        >
           {movies.map((movie) => (
             <Thumbnail key={movie.id} movie={movie} />
           ))}
         </div>
-        <ChevronRightIcon
-          className="absolute top-0 bottom-0 left-2 z-40 m-auto h-9 w-9
-         cursor-pointer opacity-0 transition hover:scale-125 group-hover:opacity-100"
-        />
+        <button onClick={() => handleClick("right")}>
+          <ChevronRightIcon
+            className="absolute top-0 bottom-0 right-2 z-40 m-auto h-9 w-9 cursor-pointer opacity-0
+             transition hover:scale-125 group-hover:opacity-100"
+          />
+        </button>
       </div>
     </div>
   );
