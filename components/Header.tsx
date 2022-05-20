2dcf5e43e28017a54c0776359492603e5ad1ac9b
zzkhotivari <zuukhotivari@gmail.com>
2022-05-20 18:40:54 +0400

    feat: temporary logout funcitonal

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,9 +1,12 @@
 import { useState, useEffect } from "react";
 import { BellIcon, SearchIcon } from "@heroicons/react/solid";
 import Link from "next/link";
+import useAuth from "../hooks/useAuth";
 
 function Header() {
   const [isScrolled, setIsScrolled] = useState(false);
+
+  const { logOut } = useAuth();
 
   useEffect(() => {
     const handleScroll = () => {
@@ -43,13 +46,15 @@ function Header() {
         <SearchIcon className="hidden h-6 w-6 sm:inline" />
         <p className="hidden lg:inline">Kids</p>
         <BellIcon className="h-6 w-6 " />
-        <Link href="/account">
+        {/*<Link href="/account">*/}
+        <button onClick={logOut}>
           <img
             src="https://rb.gy/g1pwyx"
             alt="user"
             className="cursor-pointer rounded"
           />
-        </Link>
+        </button>
+        {/*</Link>*/}
       </div>
     </header>
   );
