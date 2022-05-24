3cd7705db3711ff77368fe8915e3a067ff05d80a
zzkhotivari <zuukhotivari@gmail.com>
2022-05-24 16:20:55 +0400

    style: modal styles

--- components/Modal.tsx
+++ components/Modal.tsx
@@ -129,12 +129,14 @@ function Modal() {
     <MuiModal
       open={showModal}
       onClose={handleClose}
-      className="fixed !top-7 left-0 right-0 z-50 mx-auto w-full max-w-5xl overflow-hidden overflow-y-scroll rounded-md scrollbar-hide"
+      className="fixed !top-7 left-0 right-0 z-50 mx-auto w-full max-w-5xl 
+      overflow-hidden overflow-y-scroll rounded-md scrollbar-hide"
     >
       <>
         <Toaster position="bottom-center" />
         <button
-          className="modalButton absolute right-5 top-5 !z-40 h-9 w-9 border-none bg-[#181818] hover:bg-[#181818]"
+          className="modalButton absolute right-5 top-5 !z-40 h-9 w-9 
+          border-none bg-[#181818] hover:bg-[#181818]"
           onClick={handleClose}
         >
           <XIcon className="h-6 w-6" />
