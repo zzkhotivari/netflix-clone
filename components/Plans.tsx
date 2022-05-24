962a92f6ff738b5582f1b8fd3c57c195e366949e
zzkhotivari <zuukhotivari@gmail.com>
2022-05-24 17:18:59 +0400

    feat: added real data and stripe functionality

--- components/Plans.tsx
+++ components/Plans.tsx
@@ -1,16 +1,29 @@
+import { useState } from "react";
 import Head from "next/head";
 import Link from "next/link";
 import Table from "./Table";
+import Loader from "./Loader";
 import useAuth from "../hooks/useAuth";
 import { Product } from "@stripe/firestore-stripe-payments";
 import { CheckIcon } from "@heroicons/react/outline";
+import { loadCheckout } from "../lib/stripe";
 
 interface IProps {
   products: Product[];
 }
 
 function Plans({ products }: IProps) {
-  const { logOut } = useAuth();
+  const [selectedPlan, setSelectedPlan] = useState<Product | null>(products[2]);
+  const [billingLoading, setBillingLoading] = useState(false);
+
+  const { logOut, user } = useAuth();
+
+  const subscribeToPlan = () => {
+    if (!user) return;
+
+    loadCheckout(selectedPlan?.prices[0].id!)
+    setBillingLoading(true)
+  };
 
   return (
     <div>
@@ -35,7 +48,7 @@ function Plans({ products }: IProps) {
           Sign Out
         </button>
       </header>
-      <main className="max-w-5xl px-5 pt-28 pb-12 transition-all">
+      <main className="mx-auto max-w-5xl px-5 pt-28 pb-12 transition-all">
         <h1 className="mb-3 text-3xl font-medium">
           Choose the plan that's right for you
         </h1>
@@ -56,11 +69,32 @@ function Plans({ products }: IProps) {
         <div className="mt-4 flex flex-col space-y-4">
           <div className="flex w-full items-center justify-center self-end md:w-3/5">
             {products.map((product) => (
-              <div key={product.id} className="planBox">{product.name}</div>
+              <div
+                key={product.id}
+                onClick={() => setSelectedPlan(product)}
+                className={`planBox ${
+                  selectedPlan?.id === product.id ? "opacity-100" : "opacity-60"
+                }`}
+              >
+                {product.name}
+              </div>
             ))}
           </div>
-          <Table />
-          <button>Subscribe</button>
+          <Table products={products} selectedPlan={selectedPlan} />
+          <button
+            onClick={subscribeToPlan}
+            disabled={!selectedPlan || billingLoading}
+            className={`mx-auto w-11/12 rounded bg-[#E50914] py-4 text-xl
+             shadow hover:bg-[#f6121d] md:w-[420px] ${
+               billingLoading && "opacity-60"
+             }`}
+          >
+            {billingLoading ? (
+              <Loader color="dark:fill-gray-900" />
+            ) : (
+              "Subscribe"
+            )}
+          </button>
         </div>
       </main>
     </div>
