430ccd74896b1f5101e011898aeb412cfb71cfa1
zzkhotivari <zuukhotivari@gmail.com>
2022-05-24 19:44:19 +0400

    feat: goToBillingPortal func with stripe

--- lib/stripe.ts
+++ lib/stripe.ts
@@ -1,3 +1,4 @@
+import { getFunctions, httpsCallable } from "@firebase/functions";
 import {
   getStripePayments,
   createCheckoutSession,
@@ -19,5 +20,19 @@ const loadCheckout = async (priceId: string) => {
     .catch((error) => console.log(error.message));
 };
 
-export { loadCheckout };
+const goToBillingPortal = async () => {
+  const instance = getFunctions(app, "us-central1");
+  const functionRef = httpsCallable(
+    instance,
+    "ext-firestore-stripe-payments-createPortalLink",
+  );
+
+  await functionRef({
+    returnUrl: `${window.location.origin}/account`,
+  })
+    .then(({ data }: any) => window.location.assign(data.url))
+    .catch((error) => console.log(error.message));
+};
+
+export { loadCheckout, goToBillingPortal };
 export default payments;
