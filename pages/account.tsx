94a1af8fa2d1d3af0133c53522ccdb518494fa4e
zzkhotivari <zuukhotivari@gmail.com>
2022-05-25 13:17:33 +0400

    feat: finished account page UI and Membership.tsx with dynamic and static info

--- pages/account.tsx
+++ pages/account.tsx
@@ -49,13 +49,15 @@ function Account({ products }: IProps) {
             </p>
           </div>
         </div>
+
         <Membership />
+
         <div
-          className="mt-6 grid grid-cols-1 gap-x-4 border px-4 py-4 md:grid-cols-4
-         md:border-x-0 md:border-t md:border-b-0 md:px-0 md:pb-0"
+          className="mt-6 grid grid-cols-1 gap-x-4 border px-4 py-4
+        md:grid-cols-4 md:border-x-0 md:border-t md:border-b-0 md:px-0 md:pb-0"
         >
-          <h4>Plan Details</h4>
-          <div>
+          <h4 className="text-lg text-[gray]">Plan Details</h4>
+          <div className="col-span-2 font-medium">
             {
               products.filter(
                 (product) => product.id === subscription?.product,
@@ -63,7 +65,7 @@ function Account({ products }: IProps) {
             }
           </div>
           <p className="cursor-pointer text-blue-500 hover:underline md:text-right">
-            Change Plan
+            Change plan
           </p>
         </div>
         <div
